b3b9b4a92534d65f29034d983155f51128ea56e6
Sasakid <24391001+Sasakid0123@users.noreply.github.com>
2021-10-03 17:14:46 +0900

    [test]CMAC関数の置き換え

--- node.js
+++ node.js
@@ -1,11 +1,39 @@
 module.exports = function(RED) {
     function SesameNode(config) {
         RED.nodes.createNode(this,config);
+
+        var apikey = this.credentials.apikey;
         var node = this;
         node.on('input', function(msg) {
-            msg.payload = msg.payload.toLowerCase();
+
+            let key_secret_hex = msg.payload.sk
+
+            let base64_history = Buffer.from(msg.user).toString('base64');
+            let key = Buffer.from(key_secret_hex, 'hex')
+
+            const date = Math.floor(Date.now() / 1000);
+            const dateDate = Buffer.allocUnsafe(4);
+            dateDate.writeUInt32LE(date);
+            const message = Buffer.from(dateDate.slice(1, 4));
+
+            let sign = context.global.aesCmac.aesCmac(key, message)
+
+            let data = {
+                'cmd':msg.cmd,
+                'history':base64_history,
+                'sign':sign,
+                'apikey':apikey
+            }
+
+            msg.uuid = msg.payload.uuid;
+            msg.payload = data;
+
             node.send(msg);
         });
     }
-    RED.nodes.registerType("sesame",SesameNode);
+    RED.nodes.registerType("sesame",SesameNode,{
+        credentials: {
+            apikey: {type:"password"}
+        }
+    });
 }
