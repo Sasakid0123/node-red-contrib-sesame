22ed84298b8a828f976722d7c27362d76415617c
Sasakid <24391001+Sasakid0123@users.noreply.github.com>
2021-10-05 22:06:54 +0900

    取得機能にリファクタリング
    誤記修正
    
    誤記修正

--- nodes/get/get.js
+++ nodes/get/get.js
@@ -1,89 +1,37 @@
-module.exports = function(RED) {
-    var aesCmac = require('node-aes-cmac');
-    var request = require('request');
-    var hostname = 'https://app.candyhouse.co';
-
-    // function SesameApiKeyNode(n) {
-    //     RED.nodes.createNode(this, n);
-    // }
+module.exports = (RED) => {
+    'use strict';
     
-    // RED.nodes.registerType('sesame-api-key', SesameApiKeyNode, {credentials: {apikey: {type: 'password'}}});
-    
+    const axiosBase = require('axios');
 
-    const cmac = (secretkey) => {
-        const key = Buffer.from(secretkey, 'hex')
-        const date = Math.floor(Date.now() / 1000);
-        const dateDate = Buffer.allocUnsafe(4)
-        dateDate.writeUInt32LE(date);
-        const message = Buffer.from(dateDate.slice(1, 4));
+    const main = function(config){
+        const node = this;
+        RED.nodes.createNode(node, config);
+        const API_KEY = node.credentials.apikey;
+        const UUID = node.credentials.uuid;
+        const axios = axiosBase.create({
+            baseURL: `https://app.candyhouse.co`,
+            headers: {
+                'x-api-key': API_KEY,
+                'user-agent': 'python-requests/2.25.1',
+            }
+        });
 
-        return (aesCmac.aesCmac(key, message));
-    }
-
-    const rqt = (options) => {
-        return new Promise((resolve, reject)=> {
-            request(options, (error, response, body)=> {
-                resolve(body);
-            });
+        node.on('input', async (msg) => {
+            const mes = msg.payload;
+            try {
+                const res = await axios.get(`/api/sesame2/${UUID}`);
+                msg.payload = res.data;
+                node.send(msg);                
+            } catch (error) {
+                console.log(error);
+            }
         });
     }
 
-    function SesameNode(config) {
-        RED.nodes.createNode(this,config);
-
-        var node = this;
-	    const uuid = node.credentials.uuid;
-        const header = {
-            "x-api-key": node.credentials.apikey,
-            "user-agent": "python-requests/2.25.1",
-            "accept-encoding": "gzip, deflate",
-            "accept": "*/*"     
-        }
-        const sign = cmac(node.credentials.secretkey);
-
-        node.on('input', function(msg) {    
-            var gethistory = {
-                url: hostname + '/api/sesame2/' + uuid,
-                headers: header,
-                method: 'GET',
-                json: true
-            };
-
-            (async () => { 
-                const body = await rqt(gethistory)
-                console.log(body);
-                
-                var cmd;
-                if (body.CHSesame2Status == 'unlocked'){
-                    cmd = 82    //lock
-                } else if (body.CHSesame2Status == 'locked') {
-                    cmd = 83    //unlock
-                }
-                var Control = {
-                    url: hostname + '/api/sesame2/' + uuid + '/cmd',
-                    headers: header,
-                    method: 'POST',
-                    json: {
-                        'cmd':cmd,
-                        'history':Buffer.from(msg.user).toString('base64'),
-                        'sign':sign
-                    }
-                };
-                console.log(Control);
-                (async () => { 
-                    const body = await rqt(Control)
-                    console.log(body);
-                    node.send(body);
-                })();
-            })();
-
-        });
-    }
-    RED.nodes.registerType("sesame",SesameNode, {
+    RED.nodes.registerType("GetStatus", main, {
         credentials: {
             apikey:{type:"password"},
-            uuid: {type:"text"},
-            secretkey: {type:"password"}
+            uuid: {type:"text"}
         }
     });
 }
