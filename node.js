6fd9d197f1d525750a8569ade5ef669b7ad651a0
Sasakid <24391001+Sasakid0123@users.noreply.github.com>
2021-10-05 20:55:29 +0900

    トグル操作成功

--- node.js
+++ node.js
@@ -10,46 +10,72 @@ module.exports = function(RED) {
     // RED.nodes.registerType('sesame-api-key', SesameApiKeyNode, {credentials: {apikey: {type: 'password'}}});
     
 
+    const cmac = (secretkey) => {
+        const key = Buffer.from(secretkey, 'hex')
+        const date = Math.floor(Date.now() / 1000);
+        const dateDate = Buffer.allocUnsafe(4)
+        dateDate.writeUInt32LE(date);
+        const message = Buffer.from(dateDate.slice(1, 4));
+
+        return (aesCmac.aesCmac(key, message));
+    }
+
+    const rqt = (options) => {
+        return new Promise((resolve, reject)=> {
+            request(options, (error, response, body)=> {
+                resolve(body);
+            });
+        });
+    }
+
     function SesameNode(config) {
         RED.nodes.createNode(this,config);
 
-
         var node = this;
-	    var apikey = this.credentials.apikey;
-	    var uuid = this.credentials.uuid;
-	    var secretkey = this.credentials.secretkey;
-        node.on('input', function(msg) {
+	    const uuid = node.credentials.uuid;
+        const header = {
+            "x-api-key": node.credentials.apikey,
+            "user-agent": "python-requests/2.25.1",
+            "accept-encoding": "gzip, deflate",
+            "accept": "*/*"     
+        }
+        const sign = cmac(node.credentials.secretkey);
 
-            let key_secret_hex = secretkey
-
-            let base64_history = Buffer.from(msg.user).toString('base64');
-            let key = Buffer.from(key_secret_hex, 'hex')
-
-            const date = Math.floor(Date.now() / 1000);
-            const dateDate = Buffer.allocUnsafe(4);
-            dateDate.writeUInt32LE(date);
-            const message = Buffer.from(dateDate.slice(1, 4));
-
-            let sign = aesCmac.aesCmac(key, message)
-
-            var header = {
-                "x-api-key": apikey,
-                "user-agent": "python-requests/2.25.1"        
-            }
-    
-            var options = {
+        node.on('input', function(msg) {    
+            var gethistory = {
                 url: hostname + '/api/sesame2/' + uuid,
                 headers: header,
                 method: 'GET',
                 json: true
             };
-            request(options, function (error, response, body) {
-                if (!error) {
-                    node.send(body);
-                } else {
-                    node.error(error);
+
+            (async () => { 
+                const body = await rqt(gethistory)
+                console.log(body);
+                
+                var cmd;
+                if (body.CHSesame2Status == 'unlocked'){
+                    cmd = 82    //lock
+                } else if (body.CHSesame2Status == 'locked') {
+                    cmd = 83    //unlock
                 }
-            });
+                var Control = {
+                    url: hostname + '/api/sesame2/' + uuid + '/cmd',
+                    headers: header,
+                    method: 'POST',
+                    json: {
+                        'cmd':cmd,
+                        'history':Buffer.from(msg.user).toString('base64'),
+                        'sign':sign
+                    }
+                };
+                console.log(Control);
+                (async () => { 
+                    const body = await rqt(Control)
+                    console.log(body);
+                    node.send(body);
+                })();
+            })();
 
         });
     }
@@ -60,5 +86,4 @@ module.exports = function(RED) {
             secretkey: {type:"password"}
         }
     });
-
 }
