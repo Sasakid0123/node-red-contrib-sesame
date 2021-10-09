d210d4e0ac428bd7b6c9cf42740b3e8f803b0644
yushiro_1121 <kuromi2kinako@gmail.com>
2021-10-09 17:08:30 +0900

    Remove trailing-spaces

--- nodes/control/control.js
+++ nodes/control/control.js
@@ -3,14 +3,14 @@ module.exports = function(RED) {
 
     const aesCmac = require('node-aes-cmac');
     const axiosBase = require('axios');
-    
+
     const cmac = (secretkey) => {
         const key = Buffer.from(secretkey, 'hex')
         const date = Math.floor(Date.now() / 1000);
         const dateDate = Buffer.allocUnsafe(4)
         dateDate.writeUInt32LE(date);
         const message = Buffer.from(dateDate.slice(1, 4));
-        
+
         return (aesCmac.aesCmac(key, message));
     }
 
@@ -20,18 +20,18 @@ module.exports = function(RED) {
         const API_KEY = RED.nodes.getNode(config.apikey).credentials.apikey;
         const UUID = RED.nodes.getNode(config.sesame).uuid;
         const SECRET_KEY = RED.nodes.getNode(config.sesame).credentials.secretkey;
-        
+
         const axios = axiosBase.create({
             baseURL: `https://app.candyhouse.co`,
             headers: {
                 'x-api-key': API_KEY,
                 'user-agent': 'python-requests/2.25.1',
                 "accept-encoding": "gzip, deflate",
-                "accept": "*/*"     
+                "accept": "*/*"
             },
             json:true
         });
-        
+
         node.on('input', async (msg) => {
             let cmd
             if (msg.payload.cmd === 'lock'){
@@ -58,7 +58,7 @@ module.exports = function(RED) {
                     'sign': cmac(SECRET_KEY)
                 });
                 msg.payload = res.data;
-                node.send(msg);                
+                node.send(msg);
             } catch (error) {
                 console.log(error);
                 node.error(error);
